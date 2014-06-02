4de4f9f5632700663e1f6a1c5c740cf8fb4d45a7
numinix <jlew@numinix.com>
2014-06-02 16:31:40 -0700

    fixed HTML error in documentation

--- install.sql
+++ install.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS shipping_boxes_manager; 
 CREATE TABLE IF NOT EXISTS shipping_boxes_manager (
   `box_id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NULL DEFAULT NULL,
@@ -14,7 +13,6 @@ ALTER TABLE products ADD nestable tinyint(1) NULL;
 ALTER TABLE products ADD nestable_percentage tinyint(3) NULL;
 ALTER TABLE products ADD nestable_group_code varchar(32) NULL; 
 
-DROP TABLE IF EXISTS products_nesting_groups;
 CREATE TABLE IF NOT EXISTS products_nesting_groups (
   `grouping_id` int NOT NULL AUTO_INCREMENT,
   `group_code` varchar(32) NULL DEFAULT NULL,
@@ -35,33 +33,16 @@ INSERT INTO configuration_group (configuration_group_id, configuration_group_tit
 SET @configuration_group_id=last_insert_id();
 UPDATE configuration_group SET sort_order = @configuration_group_id WHERE configuration_group_id = @configuration_group_id;
 
-INSERT INTO configuration (configuration_id, configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function) VALUES
-(NULL, 'Version', 'SHIPPING_BOXES_MANAGER_VERSION', '2.2.0', 'Version Installed:', @configuration_group_id, 0, NOW(), NULL, NULL),
-(NULL, 'Status', 'MODULE_SHIPPING_BOXES_MANAGER_STATUS', 'false', 'Use shipping boxes manager to calculate package weights/dimensions?', @configuration_group_id, 1, NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
-(NULL, 'Debug', 'MODULE_SHIPPING_BOXES_MANAGER_DEBUG', 'false', 'Enable debug mode output?', @configuration_group_id, 2, NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),');
+INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, use_function, set_function) VALUES
+('Version', 'SHIPPING_BOXES_MANAGER_VERSION', '2.2.0', 'Version Installed:', @configuration_group_id, 0, NOW(), NULL, NULL),
+('Status', 'MODULE_SHIPPING_BOXES_MANAGER_STATUS', 'false', 'Use shipping boxes manager to calculate package weights/dimensions?', @configuration_group_id, 1, NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),'),
+('Debug', 'MODULE_SHIPPING_BOXES_MANAGER_DEBUG', 'false', 'Enable debug mode output?', @configuration_group_id, 2, NOW(), NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),');
                                      
 #Register the configuration page for Admin Access Control
 INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('configShippingBoxesManager', 'BOX_SHIPPING_BOXES_MANAGER_CONFIGURATION', 'FILENAME_CONFIGURATION',CONCAT('gID=',@configuration_group_id), 'configuration', 'Y', @configuration_group_id);
 
 # Register the tools page for Admin Access Control
-INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('toolsShippingBoxesManager', 'BOX_SHIPPING_BOXES_MANAGER', 'FILENAME_SHIPPING_BOXES_MANAGER', '', 'tools', 'Y', 31);
+INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('toolsShippingBoxesManager', 'BOX_SHIPPING_BOXES_MANAGER', 'FILENAME_SHIPPING_BOXES_MANAGER', '', 'tools', 'Y', @configuration_group_id);
 
 # Register the tools page for Admin Access Control
-INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('catalogProductsNestingManager', 'BOX_PRODUCTS_NESTING_MANAGER', 'FILENAME_PRODUCTS_NESTIN_MANAGER', '', 'tools', 'Y', 31);
-
-# Update to 2.2.0
-
-ALTER TABLE products ADD nestable_group_code varchar(32) NULL; 
-DROP TABLE IF EXISTS products_nesting_groups;
-CREATE TABLE IF NOT EXISTS products_nesting_groups (
-  `grouping_id` int NOT NULL AUTO_INCREMENT,
-  `group_code` varchar(32) NULL DEFAULT NULL,
-  `compatible_group_code` varchar(32) NULL DEFAULT NULL,
-  `nesting_percentage` float(3) NULL DEFAULT NULL,
-   PRIMARY KEY ( `grouping_id` )
-);
-
-# Register the tools page for Admin Access Control
-INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('catalogProductsNestingManager', 'BOX_PRODUCTS_NESTING_MANAGER', 'FILENAME_PRODUCTS_NESTIN_MANAGER', '', 'tools', 'Y', 31);
-
-UPDATE configuration SET configuration_value = '2.2.0' WHERE configuration_key = 'SHIPPING_BOXES_MANAGER_VERSION' LIMIT 1;
+INSERT IGNORE INTO admin_pages (page_key, language_key, main_page, page_params, menu_key, display_on_menu, sort_order) VALUES ('catalogProductsNestingManager', 'BOX_PRODUCTS_NESTING_MANAGER', 'FILENAME_PRODUCTS_NESTIN_MANAGER', '', 'tools', 'Y', @configuration_group_id);
--- uninstall.sql
+++ uninstall.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS shipping_boxes_manager;
+# Uncommenting the two lines below will result in permanent data loss:
+#DROP TABLE IF EXISTS shipping_boxes_manager;
+#DROP TABLE IF EXISTS products_nesting_groups;
 
 SELECT (@configuration_group_id:=configuration_group_id) 
 FROM configuration_group 
@@ -10,3 +12,4 @@ DELETE FROM configuration_group WHERE configuration_group_id = @configuration_gr
 #Zen Cart v1.5.0+ only Below! Skip if using an older version!
 DELETE FROM admin_pages WHERE page_key = 'toolsShippingBoxesManager' LIMIT 1;
 DELETE FROM admin_pages WHERE page_key = 'configShippingBoxesManager' LIMIT 1;
+DELETE FROM admin_pages WHERE page_key = 'catalogProductsNestingManager' LIMIT 1;
